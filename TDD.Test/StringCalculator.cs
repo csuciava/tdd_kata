f541f35643abe60a4a2836d17a92174540f62858
OMEGA\Cristian <cristian.suciava@gmail.com>
2018-03-28 20:54:54 +0300

    /n

--- TDD.Test/StringCalculator.cs
+++ TDD.Test/StringCalculator.cs
@@ -6,11 +6,17 @@ namespace TDD.Test
     {
         internal int Sum(string str)
         {
+            if (str.IndexOf("\n") > 0)
+            {
+                str= str.Replace("\n", ",");
+            }
+
             var arrayOfNumbers = str.Split(',');
             var result = 0;
             foreach (var strNumber in arrayOfNumbers)
             {
                 var number = 0;
+
                 if (int.TryParse(strNumber, out number))
                 {
                     if (number > 0) result += number;
